4effd1e5e0c12ef6ee872c237f73641edc516d72
Rohit <yararohit8@gmail.com>
2026-08-05 12:48:59 +0530

    Update CORS

--- Backend/index.js
+++ Backend/index.js
@@ -6,25 +6,34 @@ const fileRoutes = require("./routes/fileRoutes");
 
 const connectDB = require("./config/db");
 
-dotenv.config();    
+dotenv.config();
 
 connectDB();
 
-const app= express();
+const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://file-sharing-707yroflt-spider9819s-projects.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/files",fileRoutes)
+app.use("/api/files", fileRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("File sharing API is Running");
+app.get("/", (req, res) => {
+  res.send("File sharing API is Running");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
